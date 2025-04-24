ee573ea7d70718c88a6f248e3a3decdb6a163683
Adnyx <theo.dub@yahoo.com>
2025-04-24 18:43:35 +0200

    FIX: forgot to install p10k

--- script.sh
+++ script.sh
@@ -12,8 +12,10 @@ while true; do
 			# Install OhMyZSH
 			sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-			# Two cool ohmyzsh plugins
+			# Cool ohmyzsh plugins
 			ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+			git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 			git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 			git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
